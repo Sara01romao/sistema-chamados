45b7e400a721f4ce886b056fc78cc182542192ae
Sara Romão <sara-romao@live.com>
2021-11-07 12:32:23 -0300

    Criação do layout da pg do login

--- src/pages/SingnIn/index.js
+++ src/pages/SingnIn/index.js
@@ -1,8 +1,36 @@
 
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './signin.css';
+import logo from '../../assets/logo.png';
+
 function SingIn(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();//para não atualizar pg
+        alert("envou")
+    }
+
+
     return(
-        <div>
-            pg de login
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Sistema Logo"/>
+                </div>
+
+                <form onSubmit={handleSubmit}>
+                    <h1>Entrar</h1>
+                    <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button type="submit">Acessar</button>
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+            </div>
+            
         </div>
     )
 }
