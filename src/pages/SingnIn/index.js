ac1c11f2e2fad68fb054b3cf35e91d9cd7273d95
Sara Romão <sara-romao@live.com>
2021-11-21 22:32:13 -0300

    Adicionado loading

--- src/pages/SingnIn/index.js
+++ src/pages/SingnIn/index.js
@@ -1,5 +1,6 @@
 
-import {useState} from 'react';
+import {useState, useContext} from 'react';
+import {AuthContext} from '../../contexts/auth'
 import {Link} from 'react-router-dom';
 import './signin.css';
 import logo from '../../assets/logo.png';
@@ -8,9 +9,14 @@ function SingIn(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const {signIn, loadingAuth} = useContext(AuthContext);
+
     function handleSubmit(e){
         e.preventDefault();//para não atualizar pg
-        alert("envou")
+        
+        if(email !=='' && password !== ''){
+            signIn(email, password)
+        }
     }
 
 
@@ -25,7 +31,7 @@ function SingIn(){
                     <h1>Entrar</h1>
                     <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <input type="password" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    <button type="submit">Acessar</button>
+                    <button type="submit">{loadingAuth ? 'Carregando...' : 'Acessar'}</button>
                 </form>
 
                 <Link to="/register">Criar uma conta</Link>
